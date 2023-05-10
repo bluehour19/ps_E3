87acd5b7194970b972c1c68bd7b3b02bcecf138d
bluehour19 <105144782+bluehour19@users.noreply.github.com>
2023-05-10 16:51:06 +0300

    Update lab4.R

--- lab4/lab4.R
+++ lab4/lab4.R
@@ -1,3 +1,34 @@
+#ex1
+parabola_area = function(N) {
+  count = 0;
+  for (i in 1:N) {
+    x = runif(1, 0, 2);
+    y = runif(1, 0, 9/8);
+    if (y <= -2*x^2 + 5*x - 2)
+      count = count + 1;
+  }
+  return (2*9/8*count/N);
+}
+#ex2
+MC_improved_integration = function(N) 
+  {
+  sum = 0;
+  for(i in 1:N) {
+    u = rexp(1, 3);
+    sum = sum + exp(-2*u*u*2);
+  }
+  return(sum/N);
+}
+MC_imprvd_integr_average= function(k, N) 
+{
+  estimates = 0;
+  for(i in 1:k)
+    estimates[i] = MC_improved_integration(N);
+  print(mean(estimates));
+  print(sd(estimates));
+}
+
+#ex3
 lambda1 = 4
 lambda2 = 12
 U = runif(1)
